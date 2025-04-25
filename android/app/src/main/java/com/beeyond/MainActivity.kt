43653c4817ec2241ccd41a4a3ee9d187e7e4ce91
dhiraj <dhirajmymodak22@gmail.com>
2025-04-25 14:19:42 +0530

    splash screen initiated

--- android/app/src/main/java/com/beeyond/MainActivity.kt
+++ android/app/src/main/java/com/beeyond/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
   
    override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme)
     super.onCreate(null)
   }
 }
